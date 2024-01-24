b20d1b987cbaaf8ee457bad007c35e3192b9808a
morabessoneorg <mora@setandforget.io>
2024-01-24 13:52:21 -0300

    students dashboard view

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,10 +1,13 @@
-import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
 import { useNavigate } from "react-router-dom";
 
 export default function StudentCard({ people }) {
   const navigate = useNavigate();
 
-  const handleNavigate = (studentId) => navigate(`/profile/${studentId}`) 
+  const handleNavigate = (studentId) => {
+    console.log(studentId);
+    navigate(`/profile/${studentId}`);
+  };
 
   return (
     <ul
@@ -19,42 +22,40 @@ export default function StudentCard({ people }) {
         >
           <div className="flex min-w-0 gap-x-4">
             <span className="h-12 w-12 flex justify-center items-center rounded-full bg-nextcolor text-white">
-              {person.name.split(" ")[0].charAt(0)}  {person.name.split(" ")[1].charAt(0)}
+              {person.name.split(" ")[0].charAt(0)}{" "}
+              {person.name.split(" ")[1].charAt(0)}
             </span>
             <div className="min-w-0 flex-auto">
               <p className="text-sm font-semibold leading-6 text-gray-900">
-                <a href={person.href}>
+                <a
+                  href={person.href}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleNavigate(person.code);
+                  }}
+                >
                   <span className="absolute inset-x-0 -top-px bottom-0" />
                   {person.name}
                 </a>
               </p>
               <p className="mt-1 flex text-xs leading-5 text-gray-500">
-                <a href={`mailto:${person.email}`} className="relative truncate hover:underline">
+                <a
+                  href={`mailto:${person.email}`}
+                  className="relative truncate hover:underline"
+                >
                   {person.email}
                 </a>
               </p>
             </div>
           </div>
           <div className="flex shrink-0 items-center gap-x-4">
-            {/* <div className="hidden sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm leading-6 text-gray-900">{person.role}</p>
-              {person.lastSeen ? (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
-                </p>
-              ) : (
-                <div className="mt-1 flex items-center gap-x-1.5">
-                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                  </div>
-                  <p className="text-xs leading-5 text-gray-500">Online</p>
-                </div>
-              )}
-            </div> */}
-            <ChevronRightIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+            <ChevronRightIcon
+              className="h-5 w-5 flex-none text-gray-400"
+              aria-hidden="true"
+            />
           </div>
         </li>
       ))}
     </ul>
-  )
+  );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,127 +6,171 @@ import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../context/AuthProvider";
 
 export default function Login() {
-    const [error, setError] = useState();
-    const [storedUserRole, setStoredUserRole] = useState(null);
-    const emailRef = useRef();
-    const passwordRef = useRef();
+  const [error, setError] = useState();
+  const [storedUserRole, setStoredUserRole] = useState(null);
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
-    const { session } = useAuthContext();
-    const navigate = useNavigate();
+  const { session } = useAuthContext();
+  const navigate = useNavigate();
 
-    async function signInWithEmail(e) {
-        e.preventDefault();
+  async function signInWithEmail(e) {
+    e.preventDefault();
 
-        let email = emailRef.current.value;
+    let email = emailRef.current.value;
 
-        const { error } = await supabase.auth.signInWithPassword({
-            email: email,
-            password: passwordRef.current.value,
-        });
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: passwordRef.current.value,
+    });
 
-        if (error) {
-            setError(JSON.parse(JSON.stringify(error)));
-        }
-
-        const { data, error: fetchError } = await supabase.from("allowed_users").select().eq("email", email);
-
-        if (data.length >= 1) {
-            localStorage.setItem("userRole", data[0].role);
-            setStoredUserRole(data[0].role);
-        }
+    if (error) {
+      setError(JSON.parse(JSON.stringify(error)));
     }
 
-    useEffect(() => {
-        if (session && storedUserRole != null) {
-            if (storedUserRole == "student") {
-                // const { data, error: fetchError } = await supabase
-                //   .from("student")
-                //   .select()
-                //   .eq("email", email);
+    const { data, error: fetchError } = await supabase
+      .from("allowed_users")
+      .select()
+      .eq("email", email);
 
-                // navigate("/profile/" + data[0].code)
-                navigate("/profile/9054");
-            } else if (storedUserRole == "teacher") {
-                navigate("/profile");
-            } else {
-                navigate("/");
+    if (data.length >= 1) {
+      localStorage.setItem("userRole", data[0].role);
+      setStoredUserRole(data[0].role);
+    }
+  }
+
+  // useEffect(() => {
+  //   if (session && storedUserRole != null) {
+  //     if (storedUserRole == "student") {
+  //       const { data, error: fetchError } = await supabase
+  //         .from("student")
+  //         .select()
+  //         .eq("email", email);
+
+  //       localStorage.setItem("studentCode", data[0].code);
+
+  //       navigate("/profile/" + data[0].code)
+  //       // navigate("/profile/9054");
+  //     } else if (storedUserRole == "teacher") {
+  //       navigate("/profile");
+  //     } else {
+  //       navigate("/");
+  //     }
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [session, storedUserRole]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (session && storedUserRole != null) {
+        if (storedUserRole === "student") {
+          try {
+            const { data, error: fetchError } = await supabase
+              .from("student")
+              .select()
+              .eq("email", emailRef.current.value);
+
+            if (fetchError) {
+              console.error("Error fetching student data:", fetchError);
+              return;
             }
+
+            if (data && data.length > 0) {
+              localStorage.setItem("studentCode", data[0].code);
+              navigate("/profile/" + data[0].code);
+            } else {
+              console.error("No student data found");
+            }
+          } catch (error) {
+            console.error("Error:", error);
+          }
+        } else if (storedUserRole === "teacher") {
+          navigate("/profile");
+        } else {
+          navigate("/");
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [session, storedUserRole]);
+      }
+    };
 
-    function signUp() {
-        navigate("/signUp");
-    }
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [session, storedUserRole]);
 
-    return (
-        <div className="flex flex-col h-screen">
-            <Header navItems={[]} />
-            <div className="flex min-h-full flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
-                <div className="w-full max-w-sm space-y-10">
-                    <div>
-                        <img
-                            className="mx-auto h-16 w-auto bg-nextcolor p-2 rounded"
-                            src="https://nextcharterschool.org/wp-content/uploads/2020/08/logo.png"
-                            alt="Your Company"
-                        />
-                        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                            Sign in to your account
-                        </h2>
-                    </div>
-                    <form className="space-y-6" onSubmit={signInWithEmail}>
-                        <div className="relative -space-y-px rounded-md shadow-sm">
-                            <div className="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-gray-300" />
-                            <div>
-                                <label htmlFor="email-address" className="sr-only">
-                                    Email address
-                                </label>
-                                <input
-                                    ref={emailRef}
-                                    id="email-address"
-                                    name="email"
-                                    type="email"
-                                    autoComplete="email"
-                                    required
-                                    className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    placeholder="Email address"
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="password" className="sr-only">
-                                    Password
-                                </label>
-                                <input
-                                    ref={passwordRef}
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    autoComplete="current-password"
-                                    required
-                                    className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    placeholder="Password"
-                                />
-                            </div>
-                        </div>
-                        <div>
-                            <button
-                                type="submit"
-                                className="flex w-full justify-center rounded-md bg-nextcolor px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Sign in
-                            </button>
-                            <button
-                                type="button"
-                                onClick={signUp}
-                                className="flex w-full justify-center rounded-md bg-transparent px-3 py-1.5 text-sm font-semibold leading-6 text-nextcolor border-1 border-nextcolor hover:opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Dont have an account? Register
-                            </button>
-                        </div>
-                        {error && <p className="text-red-500 text-center">{error.message}</p>}
-                    </form>
-                </div>
+  function signUp() {
+    navigate("/signUp");
+  }
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header navItems={[]} />
+      <div className="flex min-h-full flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+        <div className="w-full max-w-sm space-y-10">
+          <div>
+            <img
+              className="mx-auto h-16 w-auto bg-nextcolor p-2 rounded"
+              src="https://nextcharterschool.org/wp-content/uploads/2020/08/logo.png"
+              alt="Your Company"
+            />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+          <form className="space-y-6" onSubmit={signInWithEmail}>
+            <div className="relative -space-y-px rounded-md shadow-sm">
+              <div className="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-gray-300" />
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  ref={emailRef}
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  ref={passwordRef}
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Password"
+                />
+              </div>
             </div>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-nextcolor px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+              <button
+                type="button"
+                onClick={signUp}
+                className="flex w-full justify-center rounded-md bg-transparent px-3 py-1.5 text-sm font-semibold leading-6 text-nextcolor border-1 border-nextcolor hover:opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Dont have an account? Register
+              </button>
+            </div>
+            {error && (
+              <p className="text-red-500 text-center">{error.message}</p>
+            )}
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
